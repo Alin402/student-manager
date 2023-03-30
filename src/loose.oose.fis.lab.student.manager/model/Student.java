f27b1bc42ace8be9a42b270328047fa8f5610284
Student <student@b426-10.intranet.cs.upt.ro>
2023-03-30 18:50:56 +0300

    SM- 2 Implemented the Student model class

---
+++ src/loose.oose.fis.lab.student.manager/model/Student.java
@@ -0,0 +1,28 @@
+package loose.oose.fis.lab.student.manager.model;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+
+    public Student(String firstName, String lastName, int age, double meanGrade) {
+
+    }
+
+    public String toString() {
+        //toString generated implementation
+        return null;
+    }
+
+    public boolean equals(Object o) {
+        //equals generated implementation
+        return false;
+    }
+
+    public int hashCode() {
+        return 0;
+    }
+
+}
+
